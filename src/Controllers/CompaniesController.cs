acadf2957382859f35ba64fb06abd7c615972bc6
Sagar Vinayak <sagarv0@gmail.com>
2021-10-18 22:52:16 -0700

    Added a new controller CompaniesController.cs to perform index action

---
+++ src/Controllers/CompaniesController.cs
@@ -0,0 +1,41 @@
+﻿using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        public CompaniesController(IWebHostEnvironment WebHostEnvironment)
+        {
+            webHostEnvironment = WebHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            var data = GetAllData();
+            return View();
+        }
+        private string JsonFileName
+        {
+            get { return Path.Combine(this.webHostEnvironment.WebRootPath, "data", "companies.json"); }
+        }
+        private IEnumerable<CompanyModel> GetAllData()
+        {
+            using (var jsonFileReader = System.IO.File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<CompanyModel[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
