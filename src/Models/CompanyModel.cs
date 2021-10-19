fa475b9b74d1ad739ff5bcc16de5dee4f02edf15
Sagar Vinayak <sagarv0@gmail.com>
2021-10-18 22:51:29 -0700

    Created a model for company CompanyModel.cs

---
+++ src/Models/CompanyModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class CompanyModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string H1BSupport { get; set; }
+        public string JobRoleName { get; set; }
+    }
+}
