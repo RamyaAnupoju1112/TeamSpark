cc7607724169de12e5f8149fd523c185e186beea
Sagar Vinayak <sagarv0@gmail.com>
2021-10-18 23:07:02 -0700

    Added routing path to startup.cs for Companiescontroller

--- src/Startup.cs
+++ src/Startup.cs
@@ -46,6 +46,12 @@ namespace ContosoCrafts.WebSite
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.UseEndpoints(endpoints =>
             {
