c4a0f9e6479820c477ddf220b11ff64ca786a3ec
Sagar Vinayak <sagarv0@gmail.com>
2021-10-19 20:38:50 -0700

    Added companies controller CompaniesController.cs which has Index action to perform Index operation of CRUDi

--- src/Controllers/CompaniesController.cs
+++ src/Controllers/CompaniesController.cs
@@ -10,6 +10,9 @@ using System.Threading.Tasks;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
+    /// <summary>
+    /// Companies controller is used to control the CRUDi actions to be performed for companies.
+    /// </summary>
     public class CompaniesController : Controller
     {
         private readonly IWebHostEnvironment webHostEnvironment;
@@ -17,15 +20,26 @@ namespace ContosoCrafts.WebSite.Controllers
         {
             webHostEnvironment = WebHostEnvironment;
         }
+
+        /// <summary>
+        /// Index action is one of the CRUDi operation, which will get the list of all the companies
+        /// It will also have links to Create, Edit, Delete and Update any company record
+        /// </summary>
+        /// <returns>This action returns a view in which we pass the List of company model to show on view</returns>
         public IActionResult Index()
         {
             var data = GetAllData();
-            return View();
+            return View(data);
         }
         private string JsonFileName
         {
             get { return Path.Combine(this.webHostEnvironment.WebRootPath, "data", "companies.json"); }
         }
+
+        /// <summary>
+        /// GetAllData method is used to Get the list of all the companies, it parses the JSON file and converts into company model list
+        /// </summary>
+        /// <returns>List of Company Model</returns>
         private IEnumerable<CompanyModel> GetAllData()
         {
             using (var jsonFileReader = System.IO.File.OpenText(JsonFileName))
