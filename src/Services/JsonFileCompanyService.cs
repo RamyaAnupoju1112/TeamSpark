d3c4a4d240ef940dc2746de32d99a77f311c590d
RamyaAnupoju <ranupoju@horizon.csueastbay.edu>
2021-10-22 20:33:01 -0700

    Created a new file JsonFileCompanyServices.cs file

---
+++ src/Services/JsonFileCompanyService.cs
@@ -0,0 +1,74 @@
+﻿using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Services
+{
+    public class JsonFileCompanyService
+    {
+        public JsonFileCompanyService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "companies.json"); }
+        }
+        public IEnumerable<CompanyModel> GetAllData()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<CompanyModel[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+        public IEnumerable<CompanyModel> GetCompanies()
+        {
+            using (var jsonFileReader = System.IO.File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<CompanyModel[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+        public CompanyModel DeleteData(string id)
+        {
+            // Get the current set, and append the new record to it
+            var dataSet = GetAllData();
+            var data = dataSet.FirstOrDefault(m => m.Id.Equals(id));
+
+            var newDataSet = GetAllData().Where(m => m.Id.Equals(id) == false);
+
+            SaveData(newDataSet);
+
+            return data;
+        }
+        private void SaveData(IEnumerable<CompanyModel> companies)
+        {
+
+            using (var outputStream = File.Create(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<CompanyModel>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    companies
+                );
+            }
+        }
+        
+    }
+}
