71e80f0fe4e9a2130bf409974ce283922c234958
RamyaAnupoju <ranupoju@horizon.csueastbay.edu>
2021-10-22 20:26:25 -0700

    Developed a functionality to delete records from company list. Changes are done in Delete.cshtml.cs file

---
+++ src/Pages/Companies/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Companies
+{
+    /// <summary>
+    /// Manage the Delete of the data for a single record
+    /// </summary>
+    public class DeleteModel : PageModel
+    {
+        // Data middletier
+        public JsonFileCompanyService CompanyService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public DeleteModel(JsonFileCompanyService companyService)
+        {
+           CompanyService = companyService;
+        }
+
+        // The data to show, bind to it for the post
+        [BindProperty]
+        public CompanyModel Company { get; set; }
+
+        /// <summary>
+        /// REST Get request
+        /// Loads the Data
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Company  = CompanyService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
+        }
+
+        /// <summary>
+        /// Post the model back to the page
+        /// The model is in the class variable Product
+        /// Call the data layer to Delete that data
+        /// Then return to the index page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            CompanyService.DeleteData(Company.Id);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
