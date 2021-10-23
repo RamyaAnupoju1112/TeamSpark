28855ab603c2c5e77a8767485052a24fad6336b1
RamyaAnupoju <ranupoju@horizon.csueastbay.edu>
2021-10-22 20:29:07 -0700

    Changes are made to read a data from JSON file in CompaniesController.cs file

--- src/Controllers/CompaniesController.cs
+++ src/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
 ﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -15,27 +16,24 @@ namespace ContosoCrafts.WebSite.Controllers
     /// </summary>
     public class CompaniesController : Controller
     {
-        private readonly IWebHostEnvironment webHostEnvironment;
-        public CompaniesController(IWebHostEnvironment WebHostEnvironment)
+        
+        public CompaniesController(JsonFileCompanyService companyService)
         {
-            webHostEnvironment = WebHostEnvironment;
+            CompanyService = companyService;
         }
 
+        public JsonFileCompanyService CompanyService { get; set; }
         /// <summary>
         /// Index action is one of the CRUDi operation, which will get the list of all the companies
         /// It will also have links to Create, Edit, Delete and Update any company record
         /// </summary>
         /// <returns>This action returns a view in which we pass the List of company model to show on view</returns>
-        public IActionResult Index()
-        {
-            var data = GetAllData();
-            return View(data);
-        }
-        private string JsonFileName
-        {
-            get { return Path.Combine(this.webHostEnvironment.WebRootPath, "data", "companies.json"); }
-        }
-
+        //public IActionResult Index()
+        //{
+        //    var data = GetAllData();
+        //    return View(data);
+        //}
+        
         public IActionResult Edit()
         {
             
@@ -62,16 +60,10 @@ namespace ContosoCrafts.WebSite.Controllers
         /// GetAllData method is used to Get the list of all the companies, it parses the JSON file and converts into company model list
         /// </summary>
         /// <returns>List of Company Model</returns>
-        private IEnumerable<CompanyModel> GetAllData()
+        [HttpGet]
+        public IEnumerable<CompanyModel> Get()
         {
-            using (var jsonFileReader = System.IO.File.OpenText(JsonFileName))
-            {
-                return JsonSerializer.Deserialize<CompanyModel[]>(jsonFileReader.ReadToEnd(),
-                    new JsonSerializerOptions
-                    {
-                        PropertyNameCaseInsensitive = true
-                    });
-            }
+            return CompanyService.GetAllData();
         }
     }
 }
