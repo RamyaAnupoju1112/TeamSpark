aafb1800dec69a03acc82b080c47d93fd17f3606
RamyaAnupoju <ranupoju@horizon.csueastbay.edu>
2021-10-22 20:28:05 -0700

    Created Index.csthml.cs file in Companies folder

---
+++ src/Pages/Companies/Index.cshtml.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Companies
+{
+    /// <summary>
+    /// Index Page will return all the data to show
+    /// </summary>
+    public class IndexModel : PageModel
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="companyService"></param>
+        public IndexModel(JsonFileCompanyService companyService)
+        {
+            CompanyService = companyService;
+        }
+
+        // Data Service
+        public JsonFileCompanyService CompanyService { get; }
+        // Collection of the Data
+        public IEnumerable<CompanyModel> Companies { get; private set; }
+
+        /// <summary>
+        /// REST OnGet, return all data
+        /// </summary>
+        public void OnGet()
+        {
+            Companies = CompanyService.GetAllData();
+        }
+    }
+}
