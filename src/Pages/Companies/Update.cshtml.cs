578e00489a69f17da8adad21844bc3cc876f9735
Pujith Neelagiri <pujith.14@gmail.com>
2021-10-25 20:17:37 -0700

    created update.cshtml.cs file

---
+++ src/Pages/Companies/Update.cshtml.cs
@@ -0,0 +1,60 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Companies
+{
+    /// <summary>
+    /// Manage the Update of the data for a single record
+    /// </summary>
+    public class UpdateModel : PageModel
+    {
+        // Data middletier
+        public JsonFileCompanyService CompanyService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public UpdateModel(JsonFileCompanyService companyService)
+        {
+            CompanyService = companyService;
+        }
+
+        // The data to show, bind to it for the post
+        [BindProperty]
+        public CompanyModel Company { get; set; }
+
+        /// <summary>
+        /// REST Get request
+        /// Loads the Data
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Company = CompanyService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
+        }
+
+        /// <summary>
+        /// Post the model back to the page
+        /// The model is in the class variable Product
+        /// Call the data layer to Update that data
+        /// Then return to the index page
+        /// </summary>
+        /// <returns></returns>
+        /*public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            CompanyService.UpdateData(Company);
+
+            return RedirectToPage("./Index");
+        }*/
+    }
+}
