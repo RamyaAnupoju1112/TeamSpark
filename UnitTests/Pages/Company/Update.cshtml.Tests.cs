8db35108d1f04e9190a616813d3ab230b019b9f0
Sanchita Jain <sjain4@seattleu.edu>
2021-10-26 19:32:01 -0700

    added test cases to cover the update page in Update.cshtml.Test.cs file

---
+++ UnitTests/Pages/Company/Update.cshtml.Tests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Companies;
+using ContosoCrafts.WebSite.Models;
+
+namespace UnitTests.Pages.Company.Update
+{
+    public class UpdateTests
+    {
+        #region TestSetup
+        public static UpdateModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new UpdateModel(TestHelper.CompanyService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Companies()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("selinazawacki-shirt");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("Floppy Crop", pageModel.Company.Name);
+        }
+        #endregion OnGet
+
+        #region OnPost
+        [Test]
+        public void OnPost_Valid_Should_Return_Companies()
+        {
+            // Arrange
+            pageModel.Company = new CompanyModel
+            {
+                Name = "selinazawacki-moon",
+                H1BSupport= "True",
+                JobRoleName = "Software Developer"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, result.PageName.Contains("Index"));
+        }
+
+        [Test]
+        public void OnPost_InValid_Model_NotValid_Return_Page()
+        {
+            // Arrange
+
+            // Force an invalid error state
+            pageModel.ModelState.AddModelError("bogus", "bogus error");
+
+            // Act
+            var result = pageModel.OnPost() as ActionResult;
+
+            // Assert
+            Assert.AreEqual(false, pageModel.ModelState.IsValid);
+        }
+        #endregion OnPost
+    }
+}
