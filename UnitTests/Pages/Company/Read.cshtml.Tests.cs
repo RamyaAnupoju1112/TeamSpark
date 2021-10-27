bbf0a14db3a602dbaa56ec0c2d53834f8fe3a467
Sagar Vinayak <sagarv0@gmail.com>
2021-10-26 17:30:31 -0700

    Created a unit test case for Read

---
+++ UnitTests/Pages/Company/Read.cshtml.Tests.cs
@@ -0,0 +1,38 @@
+﻿using ContosoCrafts.WebSite.Pages.Companies;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Pages.Company
+{
+    public class ReadTests
+    {
+        public static ReadModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new ReadModel(TestHelper.CompanyService)
+            {
+            };
+        }
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Companies()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("111112");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("Microsoft", pageModel.Company.Name);
+        }
+        #endregion OnGet
+    }
+}
