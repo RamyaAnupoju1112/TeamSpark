ff98f019683610bfc677c207a69e9d8b6249e9dc
Pujith Neelagiri <pujith.14@gmail.com>
2021-10-26 17:42:42 -0700

    Added unit test of create functinality in Create.cshtml.tests.cs file.

---
+++ UnitTests/Pages/Company/Create.cshtml.Tests.cs
@@ -0,0 +1,42 @@
+﻿using ContosoCrafts.WebSite.Pages.Companies;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Pages.Company
+{
+    public class CreateTests
+    {
+        #region TestSetup
+        public static CreateModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new CreateModel(TestHelper.CompanyService)
+            {
+            };
+ }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Companies()
+        {
+            // Arrange
+            var oldCount = TestHelper.CompanyService.GetAllData().Count();
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(oldCount + 1, TestHelper.CompanyService.GetAllData().Count());
+        }
+        #endregion OnGet
+    }
+}
