792c188090a0f11e41535a57cef67fea98840fc0
Sanchita Jain <sjain4@seattleu.edu>
2021-11-02 14:05:37 -0700

    added comments to Read.cshtml.cs file

--- src/Pages/Companies/Read.cshtml.cs
+++ src/Pages/Companies/Read.cshtml.cs
@@ -7,12 +7,18 @@ namespace ContosoCrafts.WebSite.Pages.Companies
 {
     public class ReadModel : PageModel
     {
+        /// Defualt Construtor
         public ReadModel(JsonFileCompanyService companyService)
         {
             CompanyService = companyService;
         }
+        /// Data to show
         public CompanyModel Company { get; set; }
+
+        // Data middletier
         public JsonFileCompanyService CompanyService { get; set; }
+
+        /// REST Get request
         public void OnGet(string id)
         {
             Company = CompanyService.GetAllData().FirstOrDefault(x => x.Id == id);
