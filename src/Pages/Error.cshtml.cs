badaf12bbf1601604e4d7ee93725a5d5a710c6aa
Sanchita Jain <sjain4@seattleu.edu>
2021-11-02 19:45:09 -0700

    Added comments to Error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -6,13 +6,17 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    //  Using ResponseCache attribute to set response caching headers.
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
+        // Get or Set RequestId
         public string RequestId { get; set; }
 
+        // Checking if the RequestId is not NULL or Empty
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        // Using Ilogger to log error messages
         private readonly ILogger<ErrorModel> _logger;
 
         public ErrorModel(ILogger<ErrorModel> logger)
@@ -20,6 +24,7 @@ namespace ContosoCrafts.WebSite.Pages
             _logger = logger;
         }
 
+        // REST Get request
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
