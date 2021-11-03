074ab5d478217751a98232baa6f69fa80c1abf27
Sanchita Jain <sjain4@seattleu.edu>
2021-11-02 19:39:42 -0700

    Fixed indentation in CompaniesController.cs

--- src/Controllers/CompaniesController.cs
+++ src/Controllers/CompaniesController.cs
@@ -14,6 +14,7 @@ namespace ContosoCrafts.WebSite.Controllers
     /// <summary>
     /// Companies controller is used to control the CRUDi actions to be performed for companies.
     /// </summary>
+   
     public class CompaniesController : Controller
     {
         
@@ -23,10 +24,12 @@ namespace ContosoCrafts.WebSite.Controllers
         }
 
         public JsonFileCompanyService CompanyService { get; }
+
         /// <summary>
         /// GetAllData method is used to Get the list of all the companies, it parses the JSON file and converts into company model list
         /// </summary>
         /// <returns>List of Company Model</returns>
+        
         [HttpGet]
         public IEnumerable<CompanyModel> Get()
         {
