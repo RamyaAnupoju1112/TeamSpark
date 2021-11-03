8582e5ef7641aa7011d4da1c88d8f155a4878f2e
Sagar Vinayak <sagarv0@gmail.com>
2021-11-02 18:31:45 -0700

    Added unit test for Get method of ProductController in ProductControllerTests.cs

---
+++ UnitTests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using ContosoCrafts.WebSite.Controllers;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static ContosoCrafts.WebSite.Controllers.ProductsController;
+
+namespace UnitTests.Controllers
+{
+    public class ProductControllerTests
+    {
+        public static ProductsController testProductController;
+
+        #region TestSetup
+        [SetUp]
+        public void TestInitialize()
+        {
+            testProductController = new ProductsController(TestHelper.ProductService);
+        }
+        #endregion
+
+        [Test]
+        public void Get_Valid_Should_Return_List_Of_Products()
+        {
+            //Arrange
+            var data = testProductController.Get().ToList();
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(typeof(List<ProductModel>), data.GetType());
+        }
+
+    }
+}
