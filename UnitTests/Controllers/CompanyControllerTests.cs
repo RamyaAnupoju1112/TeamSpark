4ca1f3b45470907ca13193d1c148e6e176402fe3
Sanchita Jain <sjain4@seattleu.edu>
2021-11-02 19:10:46 -0700

    Added unit tests for Company controller in CompanyControllerTests.cs

--- UnitTests/Controllers/CompanyControllerTests.cs
+++ UnitTests/Controllers/CompanyControllerTests.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using ContosoCrafts.WebSite.Controllers;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static ContosoCrafts.WebSite.Controllers.CompaniesController;
+
 namespace UnitTests.Controllers
 {
-    public class CompanyControllerTests
+    public class CompaniesControllerTests
     {
-        public CompanyControllerTests()
+        public static CompaniesController testCompanyController;
+
+        #region TestSetup
+        [SetUp]
+        public void TestInitialize()
         {
+            testCompanyController = new CompaniesController(TestHelper.CompanyService);
         }
+        #endregion
+
+        [Test]
+        public void Get_Valid_Should_Return_List_Of_Companies()
+        {
+            //Arrange
+            var data = testCompanyController.Get().ToList();
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(typeof(List<CompanyModel>), data.GetType());
+        } 
     }
 }
