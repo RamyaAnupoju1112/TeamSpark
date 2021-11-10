2a7324f9689844544c6ce8ae56ee62f54ea4e8a2
Jasmine dhunna <jasmine9003@gmail.com>
2021-11-09 18:56:01 -0800

    Added a test case for SearchCompany in JsonFIleCompanyServiceTests.cs

---
+++ UnitTests/Services/JsonFileCompanyServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Services
+{
+    public class JsonFileCompanyServiceTests
+    {
+        #region TestSetup
+
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
+        [Test]
+        public void SearchCompany_Empty_Search_String_Returns_Companies_List()
+        {
+            //Arrange
+            var emptyString = "";
+            var companyList = TestHelper.CompanyService.GetAllData();
+
+            //Act
+            var companies = TestHelper.CompanyService.SearchCompany(emptyString);
+
+            //Assert
+            Assert.AreEqual(companyList.Count(), companies.Count());
+        }
+    }
+}
