cc042c11900b5b153f4225cd3237f11fd92bc6d5
Sagar Vinayak <svinayak@seattleu.edu>
2021-11-09 19:38:14 -0800

    Removed extra using statements from ProductList.razor.Tests.cs

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -1,11 +1,7 @@
-
-using Bunit;
 using NUnit.Framework;
-
 using ContosoCrafts.WebSite.Components;
 using Microsoft.Extensions.DependencyInjection;
 using ContosoCrafts.WebSite.Services;
-using System.Linq;
 
 namespace UnitTests.Components
 {
