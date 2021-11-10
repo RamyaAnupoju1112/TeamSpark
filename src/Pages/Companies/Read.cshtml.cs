d2a0f60819002b208d497bfec7d3478a3e26c161
RamyaAnupoju <ranupoju@seattleu.edu>
2021-11-09 22:49:05 -0800

    Added comments to Read.cshtml.cs

--- src/Pages/Companies/Read.cshtml.cs
+++ src/Pages/Companies/Read.cshtml.cs
@@ -5,22 +5,30 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages.Companies
 {
+    /// <summary>
+    /// Read page will read the data related to particular company
+    /// </summary>
     public class ReadModel : PageModel
     {
-
+        /// <summary>
         /// Defualt Construtor
+        /// </summary>
+        /// <param name="companyService"></param>
         public ReadModel(JsonFileCompanyService companyService)
         {
             CompanyService = companyService;
         }
 
-        /// Data to show
+        // Data to show
         public CompanyModel Company { get; set; }
 
         // Data middletier
         public JsonFileCompanyService CompanyService { get; set; }
 
+        /// <summary>
         /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
         public void OnGet(string id)
         {
             Company = CompanyService.GetAllData().FirstOrDefault(x => x.Id == id);
