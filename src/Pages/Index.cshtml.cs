9d897b333a0fd633900b1cf7c4e61b9b898dbc0e
Pujith Neelagiri <pujith.14@gmail.com>
2021-11-09 23:25:41 -0800

    Added comments to Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -8,11 +8,19 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Index page is loaded on the start of the application
+    /// </summary>
     public class IndexModel : PageModel
     {
-       //using Ilogger to log error messages 
+        //using Ilogger to log error messages 
         private readonly ILogger<IndexModel> _logger;
 
+        /// <summary>
+        /// Logger
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
         public IndexModel(ILogger<IndexModel> logger,
             JsonFileProductService productService)
         {
@@ -22,9 +30,13 @@ namespace ContosoCrafts.WebSite.Pages
 
         // Data middletier
         public JsonFileProductService ProductService { get; }
+
+        //get or set for Products
         public IEnumerable<ProductModel> Products { get; private set; }
 
-        /// REST Get request
+        /// <summary>
+        /// Rest Get request
+        /// </summary>
         public void OnGet()
         {
             Products = ProductService.GetAllData();
