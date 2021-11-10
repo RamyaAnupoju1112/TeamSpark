6f44479c145152d7e06964072835d2ebc638bc8e
Pujith Neelagiri <pujith.14@gmail.com>
2021-11-09 23:43:33 -0800

    Added comments to Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -1,28 +1,21 @@
-﻿using System.Diagnostics;
-
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Using ResponseCache attribute to set response caching headers.
-    /// Class is used to handle operations related to Errors
+    /// Class to handle operations related to Privacy page
     /// </summary>
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public class ErrorModel : PageModel
+    public class PrivacyModel : PageModel
     {
-        // Get or Set RequestId
-        public string RequestId { get; set; }
+        //using Ilogger to log error messages
+        private readonly ILogger<PrivacyModel> _logger;
 
-        // Checking if the RequestId is not NULL or Empty
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-        // Using Ilogger to log error messages
-        private readonly ILogger<ErrorModel> _logger;
-
-        public ErrorModel(ILogger<ErrorModel> logger)
+        /// <summary>
+        /// Logger
+        /// </summary>
+        /// <param name="logger"></param>
+        public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
@@ -32,7 +25,6 @@ namespace ContosoCrafts.WebSite.Pages
         /// </summary>
         public void OnGet()
         {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
     }
 }
