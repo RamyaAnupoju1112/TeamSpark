2e2d0b78a104a054b0de8e76d3be398e45c2247b
Pujith Neelagiri <pujith.14@gmail.com>
2021-11-09 23:25:56 -0800

    Added comments to Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -1,21 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
-    public class PrivacyModel : PageModel
+    /// <summary>
+    /// Using ResponseCache attribute to set response caching headers.
+    /// Class is used to handle operations related to Errors
+    /// </summary>
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class ErrorModel : PageModel
     {
-        //using Ilogger to log error messages
-        private readonly ILogger<PrivacyModel> _logger;
+        // Get or Set RequestId
+        public string RequestId { get; set; }
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        // Checking if the RequestId is not NULL or Empty
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        // Using Ilogger to log error messages
+        private readonly ILogger<ErrorModel> _logger;
+
+        public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
         /// REST Get request
+        /// </summary>
         public void OnGet()
         {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
     }
 }
