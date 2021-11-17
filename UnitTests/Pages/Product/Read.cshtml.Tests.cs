5d15dfe4038d776dd64b2262416dc264ab90d4b6
RamyaAnupoju <ranupoju@seattleu.edu>
2021-11-16 21:17:25 -0800

    Added comments to Read.cshtml.Tests.cs

--- UnitTests/Pages/Product/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -7,11 +7,18 @@ using ContosoCrafts.WebSite.Pages.Product;
 
 namespace UnitTests.Pages.Product.Read
 {
+    /// <summary>
+    /// Class containing unit test cases for Read page
+    /// </summary>
     public class ReadTests
     {
+        // creating an instance
         #region TestSetup
         public static ReadModel pageModel;
 
+        /// <summary>
+        /// Initializing tests
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -23,8 +30,10 @@ namespace UnitTests.Pages.Product.Read
         #endregion TestSetup
 
         #region OnGet
+        /// <summary>
+        /// Testing onget valid should return all products
+        /// </summary>
         [Test]
-        //Testing onget valid should return all products
         public void OnGet_Valid_Should_Return_Products()
         {
             // Arrange
@@ -37,8 +46,10 @@ namespace UnitTests.Pages.Product.Read
             Assert.AreEqual("The Quantified Cactus: An Easy Plant Soil Moisture Sensor", pageModel.Product.Title);
         }
 
+        /// <summary>
+        /// Testing onGet Invalid id should return to bogus page
+        /// </summary>
         [Test]
-        //Testing onGet Invalid
         public void OnGet_InValid_Id_Bougs_Should_Return_Products()
         {
             // Arrange
