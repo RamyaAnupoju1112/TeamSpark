4b31ad831b33bc009cc9884eedb70024a00cee9f
RamyaAnupoju <ranupoju@seattleu.edu>
2021-11-16 20:26:07 -0800

    Added comments to Create.cshtml.Tests.cs

--- UnitTests/Pages/Product/Create.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Create.cshtml.Tests.cs
@@ -7,11 +7,20 @@ using ContosoCrafts.WebSite.Pages.Product;
 
 namespace UnitTests.Pages.Product.Create
 {
+    /// <summary>
+    /// Class containing unit test cases of create page
+    /// </summary>
     public class CreateTests
     {
+        /// <summary>
+        /// Creating insance of the model
+        /// </summary>
         #region TestSetup
         public static CreateModel pageModel;
 
+        /// <summary>
+        /// Initializing test
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -22,7 +31,9 @@ namespace UnitTests.Pages.Product.Create
 
         #endregion TestSetup
 
-        // Testing If on get the it is returnig all the comapny names
+        /// <summary>
+        /// Testing If on get the it is returnig all the comapny names
+        /// </summary> 
         #region OnGet
         [Test]
         public void OnGet_Valid_Should_Return_Products()
